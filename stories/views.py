05c7fc0f6451084d878a6bac0959948b28ce4bfa
jstnn818 <jstnn818@gmail.com>
2024-01-26 13:54:27 -0800

    story filter desc

--- stories/views.py
+++ stories/views.py
@@ -9,7 +9,8 @@ def home(request):
     return render(request, "stories/home.html", {})
 
 def story_list_all(request):
-    story_list = Story.objects.all()
+    sort = request.GET.get('sort','title')
+    story_list = Story.objects.all().order_by(sort)
     return render(request, "stories/story_list.html", {
         'story_list': story_list,
     })
